28f3aac86fa6aee35fbe9b49cd2296e5b48e8eee
abhishek-raghav08 <abhishek.raghav@natwest.com>
2022-01-05 05:15:49 +0000

    Udacity Proj4

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,23 @@
 
 # Step 1:
 # This is your Docker ID/path
+dockerpath=abhishekraghav08/apip
 # dockerpath=<>
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+#minikube kubectl -- run apip\
+#    -- generator=apip/v1\
+#    -- image=$dockerpath\
+#    -- port=80 -- labels app=apip
+minikube kubectl -- create deployment apip --image=$dockerpath:latest
 
 
 # Step 3:
 # List kubernetes pods
+minikube kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+minikube kubectl port-forward deployment/apip 8000:80
 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,16 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=abhishekraghav08/apip
 
 # Step 2:  
 # Authenticate & tag
+
+docker login --username=abhishekraghav08
+docker tag apip $dockerpath
+
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
